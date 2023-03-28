78e68d87876e716c64ff9de220b150c3287a1323
Kamila Arlinda <125217764+camille-02@users.noreply.github.com>
2023-03-28 11:38:59 +0700

    v1.3

--- proses.php
+++ proses.php
@@ -1,4 +1,13 @@
-<form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assesment 1</title>
+</head>
+<body>
+    <form>
       <label for="angka1">Angka 1:</label>
       <input type="number" id="angka1" name="angka1"><br>
 
@@ -7,11 +16,12 @@
 
       <label for="operator">Operator:</label>
       <select id="operator" name="operator">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value=""></option>
-        <option value="/">/</option>
+              <option value="+">+</option>
+              <option value="-">-</option>
+              <option value=""></option>
+              <option value="/">/</option>
       </select><br>
-
       <input type="submit" value="Hitung">
 </form>
+</body>
+</html>
